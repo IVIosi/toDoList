d704200423b83e80f306c2fc3da337635961f851
Mostafa <mos.sameti@gmail.com>
2019-04-14 23:23:59 +0430

    add edit feature

--- js/main.js
+++ js/main.js
@@ -1,30 +1,42 @@
 "use strict";
 
 class Task {
-  constructor(title, completed) {
+  constructor(title, completed, editing) {
     this.taskTitle = title;
     this.completed = completed;
+    this.editMode = editing;
   }
   renderTask() {
     //creating inner elements
+    //main li element
     var listItem = document.createElement("li");
     listItem.setAttribute("id", this.taskTitle);
+    //main completed checkbox
     var checkBox = document.createElement("input");
     checkBox.setAttribute("type", "checkbox");
     checkBox.addEventListener("click", this.markAsCompleted.bind(this));
+    //edit box input
+    var editBox = document.createElement("input");
+    editBox.setAttribute("type", "text");
+    editBox.setAttribute("value", this.taskTitle);
+    editBox.className = "editBox";
+    //task title
     var listItemText = document.createElement("div");
     listItemText.setAttribute("class", "taskTitle");
     listItemText.textContent = this.taskTitle;
+    //edit button
     var editButton = document.createElement("button");
     editButton.addEventListener("click", this.editTask.bind(this));
     var editButtonSpan = document.createElement("span");
     editButtonSpan.textContent = "Edit";
+    //remove button
     var removeButton = document.createElement("button");
     removeButton.addEventListener("click", this.removeTask.bind(this));
     var removeButtonSpan = document.createElement("span");
     removeButtonSpan.textContent = "Remove";
     //appending all inner elements
     listItem.appendChild(checkBox);
+    listItem.appendChild(editBox);
     listItem.appendChild(listItemText);
     listItem.appendChild(editButton);
     editButton.appendChild(editButtonSpan);
@@ -42,7 +54,19 @@ class Task {
     }
   }
   editTask() {
-    console.log("edit");
+    this.editMode = !this.editMode;
+    var taskItem = document.getElementById(this.taskTitle);
+    if (this.editMode) {
+      //going to edit mode
+      taskItem.className = "editMode";
+    } else {
+      //saving changes by removing current task and add new task
+      taskItem.className = "";
+      this.taskTitle = taskItem.childNodes[1].value;
+      taskItem.parentNode.removeChild(taskItem);
+      var newTask = new Task(this.taskTitle, false, false);
+      newTask.renderTask();
+    }
   }
   removeTask() {
     var taskItem = document.getElementById(this.taskTitle);
@@ -53,12 +77,12 @@ class Task {
 const addTask = () => {
   var taskTitle = document.getElementById("newTaskTitle").value;
   if (taskTitle !== "") {
-    var newTask = new Task(taskTitle, false);
+    var newTask = new Task(taskTitle, false, false);
     newTask.renderTask();
   } else {
     alert("Task can't be empty!");
   }
 };
 
-const sampleTask = new Task("sample", false);
+const sampleTask = new Task("sample", false, false);
 sampleTask.renderTask();
